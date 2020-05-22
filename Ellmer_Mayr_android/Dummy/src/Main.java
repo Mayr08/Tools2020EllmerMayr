dc5647f5b98dbebdbd44d372a1ddd7d0a6467720
Simon Mayr <mayr97.sm@gmx.at>
2020-05-22 10:55:07 +0200

    added a few LoC

--- Ellmer_Mayr_android/Dummy/src/Main.java
+++ Ellmer_Mayr_android/Dummy/src/Main.java
@@ -9,5 +9,9 @@ public class Main {
 		System.out.println("Hello from Member 1");
 		System.out.println("Conflict :) !");
 	}
+	
+	public void NewMethod() {
+		System.out.println("I am writing a few Lines of Codes.");
+	}
 
 }
